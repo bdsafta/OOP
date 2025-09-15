7fc45c1900e584d8b6a8a73c427e86ed6577881d
bdsafta <budi_safta@smktelkom-lpg.sch.id>
2025-09-15 10:17:00 +0700

    first commit

---
+++ src/oop/Buku.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oop;
+
+/**
+ *
+ * @author MSI
+ */
+public class Buku {
+    String judul, penulis, penerbit;
+    int tahun_terbit;
+            
+            // Method
+            void infoBuku(){
+                System.out.println("Judul Buku : " + judul);
+                System.out.println("Penulis : " + penulis);
+                System.out.println("Penerbit : " + penerbit);
+                System.out.println("Tahun terbit: " + tahun_terbit);
+                
+            }
+}
---
+++ src/oop/OOP.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package oop;
+
+/**
+ *
+ * @author MSI
+ */
+public class OOP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Buku buku1 = new Buku();
+        buku1.judul ="Belajar OOP Java";
+        buku1.penulis ="Budi Nugroho";
+        buku1.penerbit ="Erlangga";
+        buku1.tahun_terbit = 2003 ;
+        
+        buku1.infoBuku();
+    }
+    
+}
